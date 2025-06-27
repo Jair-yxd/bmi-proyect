e5b8d648b4f1e91eb2033878d9914d07f798403b
Jair.yxz <jairnromero@outlook.com.ar>
2025-06-27 07:25:26 +0200

    Update bmi.c
    Primera modificacion

--- bmi.c
+++ bmi.c
@@ -12,7 +12,7 @@ int main()
     printf("Ingrese su altura en metros: ");
     scanf("%f", &altura);
     
-    imc = peso / (altura * altura);
+    imc = (peso/(altura * altura));
 
     printf("Su índice de masa corporal es: %.1f\n", imc);
 
@@ -21,5 +21,19 @@ int main()
     printf("18.5 - 24.9|Normal\n");
     printf("25.0 - 29.9| Sobrepeso\n");
     printf("30.0 o más|Obesidad\n");
+    
+    if (imc < 18.5) {
+        printf("Su condicion es: Bajo peso\n");
+    }
+    else if (imc < 25.0) {
+        printf("Su condicion es: Normal\n");
+    }
+    else if (imc < 30.0) {
+        printf("Su condicion es: Sobrepeso\n");
+    }
+    else {
+        printf("Su condicion es: Obesidad\n");
+    }
+
     return 0;
 }
